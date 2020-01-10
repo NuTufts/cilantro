359c6394c1044a915534417d8dbc3bd2b5a9306d
polabr <polabr@umich.edu>
2020-01-10 11:28:22 -0500

    change env names for larflow use

--- dllee_setup.sh
+++ dllee_setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 export CILANTRO_BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export CILANTRO_INCDIR=${CILANTRO_BASEDIR}/include
-export CILANTRO_LIBDIR=${CILANTRO_BASEDIR}/build
-export CILANTRO_BINDIR=${CILANTRO_BASEDIR}/bin
+export CILANTRO_INC_DIR=${CILANTRO_BASEDIR}/include
+export CILANTRO_LIB_DIR=${CILANTRO_BASEDIR}/build
+export CILANTRO_BIN_DIR=${CILANTRO_BASEDIR}/bin
 
 # ADD PATHS
-[[ ":$PATH:" != *":${CILANTRO_BINDIR}:"* ]] && export PATH="${CILANTRO_BINDIR}:${PATH}"
-[[ ":$LD_LIBRARY_PATH:" != *":${CILANTRO_LIBDIR}:"* ]] && export LD_LIBRARY_PATH="${CILANTRO_LIBDIR}:${LD_LIBRARY_PATH}"
+[[ ":$PATH:" != *":${CILANTRO_BIN_DIR}:"* ]] && export PATH="${CILANTRO_BIN_DIR}:${PATH}"
+[[ ":$LD_LIBRARY_PATH:" != *":${CILANTRO_LIB_DIR}:"* ]] && export LD_LIBRARY_PATH="${CILANTRO_LIB_DIR}:${LD_LIBRARY_PATH}"
 
 
